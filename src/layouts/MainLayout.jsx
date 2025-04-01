2fbd6c191597b064b6c3a6b409574436ecd67f51
heshangoonawardena <heshangoonawardena@gmail.com>
2025-04-01 20:23:50 +0530

    feat(footer): add footer component

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router";
 import Navigation from "../components/Navigation";
+import { Footer7 } from "@/components/ui/footer";
 
 const MainLayout = () => {
 
   return (
-    <>
-      <Navigation />
-      <Outlet />
-    </>
-  );
+		<>
+			<Navigation />
+			<Outlet />
+			<Footer7 />
+		</>
+	);
 };
 
 export default MainLayout;  
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,5 +1,4 @@
 import { Toaster } from "@/components/ui/sonner";
-import React from "react";
 import { Outlet } from "react-router";
 
 const RootLayout = () => {
